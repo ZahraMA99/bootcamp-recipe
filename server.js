c027083d8724aa6227dbd3ae5d212021dd527c9a
Zahra'a Aqel <zmoudmah@gmail.com>
2022-07-28 01:22:14 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Running server on port ${port}`);
+});
